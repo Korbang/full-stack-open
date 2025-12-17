2b2bcdc70cd5cd4b691c7e6b395c88e28abc4bce
David.Kuterna <david.kuterna@ife-doors.com>
2025-12-17 21:36:45 +0100

    Load data from the server

--- Part 2/phonebook/src/App.jsx
+++ Part 2/phonebook/src/App.jsx
@@ -3,14 +3,32 @@ import Filter from './Filter'
 import Persons from './Persons'
 import Form from './Form'
 import Headline from './Headline'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
+  const [persons, setPersons] = useState([])
+
+  /*
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+  */
+
+  useEffect(() => {
+    console.log('effect')
+
+    const eventHandler = response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    }
+
+    const promise = axios.get('http://localhost:3001/persons')
+    promise.then(eventHandler)
+  }, [])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
