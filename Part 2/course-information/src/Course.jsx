94c509b9f5fafc7d9c3f9444b16c39490f5a4861
David.Kuterna <david.kuterna@ife-doors.com>
2025-12-17 20:17:43 +0100

    Steps 9 and 10

--- Part 2/course-information/src/Course.jsx
+++ Part 2/course-information/src/Course.jsx
@@ -1,15 +1,42 @@
-import Header from "./Header"
-import Content from "./Content"
-import Total from "./Total"
-
-export default function Course({course}) {
-    const total = course.parts.reduce((acc, curr) => acc + curr.exercises, 0);
+export default function Course({courses}) {
 
     return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total total={total}/>
-    </div>
+        <>
+            {courses.map((course) => {
+                    const total = course.parts.reduce(
+                        (acc, curr) => acc + curr.exercises,
+                        0
+                    );
+
+                    return (
+                        <div key={course.id}>
+                            <Header course={course.name} />
+                            <Content parts={course.parts} />
+                            <Total total={total}/>
+                        </div>
+                    );
+                })
+            }
+        </>
   )
 }
+
+const Header = (props) => <h1>{props.course}</h1>
+
+const Content = (props) => (
+  <div>
+    {props.parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+);
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+
+  //{props.part.name}
+)
+
+const Total = (props) => <p><strong>total of {props.total} exercises </strong></p>
