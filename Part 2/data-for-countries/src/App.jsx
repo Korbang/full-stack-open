9e46efde508b6c47dd401ac8236ecb38d88cd392
David.Kuterna <david.kuterna@ife-doors.com>
2025-12-18 12:51:35 +0100

    2.19

--- Part 2/data-for-countries/src/App.jsx
+++ Part 2/data-for-countries/src/App.jsx
@@ -5,6 +5,7 @@ import countryService from './services/countryService';
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState(null);
 
   useEffect(() => {
     const eventHandler = countries => {
@@ -23,38 +24,27 @@ function App() {
       result.name.common.toLowerCase().includes(searchTerm.toLowerCase())
     );
 
-  console.log(filteredCountries);
+  const showCountry = (name) => {
+    const country = results.find(r => r.name.common === name);
+    setSelectedCountry(country);
+  };
+
+  const countryToShow = filteredCountries.length === 1 ? filteredCountries[0] : selectedCountry;
 
   let content;
 
-if (filteredCountries.length > 10) {
-  content = <p>Too many matches, specify another filter</p>;
-} else if (filteredCountries.length === 1) {
-  const country = filteredCountries[0];
-
-  content = (
-      <div>
-        <h2>{country.name.common}</h2>
-        <p>Capital: {country.capital?.[0]}</p>
-        <p>Area: {country.area}</p>
-
-        <h3>Languages:</h3>
-        <ul>
-          {Object.values(country.languages || {}).map(lang => (
-            <li key={lang}>{lang}</li>
-          ))}
-        </ul>
-
-        <img
-          src={country.flags.png}
-          alt={`Flag of ${country.name.common}`}
-          width="150"
-        />
-      </div>
+  if (filteredCountries.length > 10) {
+    content = <p>Too many matches, specify another filter</p>;
+  } else if (countryToShow) {
+    content = (
+      <CountryDetails country={countryToShow} />
     );
   } else {
     content = filteredCountries.map(country => (
-      <p key={country.cca3}>{country.name.common}</p>
+      <div>
+        <p key={country.cca3}>{country.name.common}</p>
+        <button type='button' onClick={() => showCountry(country.name.common)}>Show</button>
+      </div>
     ));
   }
 
@@ -76,6 +66,29 @@ function Input({name, value, handle}) {
             {name} <input value={value} onInput={handle}/>
         </div>
     );
+}
+
+function CountryDetails({ country }) {
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+      <p>Capital: {country.capital?.[0]}</p>
+      <p>Area: {country.area}</p>
+
+      <h3>Languages:</h3>
+      <ul>
+        {Object.values(country.languages || {}).map(lang => (
+          <li key={lang}>{lang}</li>
+        ))}
+      </ul>
+
+      <img
+        src={country.flags.png}
+        alt={`Flag of ${country.name.common}`}
+        width="150"
+      />
+    </div>
+  );
 }
 
 export default App
