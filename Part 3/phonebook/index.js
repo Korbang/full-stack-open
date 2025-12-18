3c40b2f0d487a20880187f557bb9e4ed5d15ab57
David.Kuterna <david.kuterna@ife-doors.com>
2025-12-18 17:35:12 +0100

    3.07

--- Part 3/phonebook/index.js
+++ Part 3/phonebook/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     { 
@@ -35,7 +38,7 @@ app.get('/api/persons/:id', (request, response) => {
     const person = persons.find(person => person.id === id)
 
     if (person) {
-        response.json(note)  
+        response.json(person)  
     } else {    
         response.status(404).end()  
     }
