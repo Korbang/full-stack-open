3ac4e3e44de6a1205ee11e1cfdcffb819204d553
David.Kuterna <david.kuterna@ife-doors.com>
2025-12-18 16:52:15 +0100

    3.05 and 3.06

--- Part 3/phonebook/index.js
+++ Part 3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": "1",
@@ -44,9 +46,38 @@ app.delete('/api/persons/:id', (request, response) => {
 
   persons = persons.filter(person => person.id !== id)
 
-  res.status(204).end()
+  response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    const nameExists = persons.some(
+        person => person.name === body.name
+    )
+
+    if (nameExists) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: Math.floor(Math.random() * 1000000),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
 
 app.get('/info', (request, response) => {
     console.log(request);
