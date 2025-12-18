8074c9e1a2a287223c10ab39af0e02779d9c7277
David.Kuterna <david.kuterna@ife-doors.com>
2025-12-18 16:40:37 +0100

    3.02

--- Part 3/phonebook/index.js
+++ Part 3/phonebook/index.js
@@ -28,6 +28,17 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    console.log(request);
+    const receivedAt = new Date()
+    const numberOfPersonsInPhoneBook = persons.length;
+
+    response.send(`
+        <p>Phonebook has info for ${numberOfPersonsInPhoneBook} people</p>
+        <p>${receivedAt}</p>
+    `);
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
