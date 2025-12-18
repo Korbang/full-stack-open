aa8f959ddfe75f6df569347b4427acfc71db16d7
David.Kuterna <david.kuterna@ife-doors.com>
2025-12-18 16:43:24 +0100

    3.03

--- Part 3/phonebook/index.js
+++ Part 3/phonebook/index.js
@@ -28,6 +28,17 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(note)  
+    } else {    
+        response.status(404).end()  
+    }
+})
+
 app.get('/info', (request, response) => {
     console.log(request);
     const receivedAt = new Date()
