204d47ddfc6f94eb26d3d74f8bf9cc5ce96a4830
David.Kuterna <david.kuterna@ife-doors.com>
2025-12-19 14:40:55 +0100

    3.12

--- Part 3/phonebook/index.js
+++ Part 3/phonebook/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
+const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 const app = express()
 
@@ -42,7 +45,9 @@ let persons = [
 ];
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
