f89ebbed9c408720e4e4ce4e1839ae63115e71f8
David.Kuterna <david.kuterna@ife-doors.com>
2025-12-19 14:05:28 +0100

    3.12

---
+++ Part 3/phonebook/mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://kuternadavid_db_user:${password}@cluster0.irjmsgb.mongodb.net/?appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({ name, number })
+
+if (!name || !number) {
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(p => console.log(`${p.name} ${p.number}`))
+    mongoose.connection.close()
+  })
+} else {
+  const person = new Person({ name, number })
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
