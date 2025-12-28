e01934828e65cb19e4da2989dd0c9ddce3eec1ef
David.Kuterna <david.kuterna@ife-doors.com>
2025-12-28 18:52:52 +0100

    4.02 Moved code to middleware.js

--- Part4/blog/app.js
+++ Part4/blog/app.js
@@ -1,38 +1,16 @@
-const config = require('./utils/config')
-const path = require('path')
 const express = require('express')
-const morgan = require('morgan')
-const logger = require('./utils/logger')
+
+const middleware = require('./utils/middleware')
 
 const app = express()
-
-morgan.token('body', (request) => {
-  return JSON.stringify(request.body)
-})
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.use(express.json())
 
 const blogsRouter = require('./controllers/blogs')
 app.use('/api/blogs', blogsRouter)
 
-app.use((request, response)=> {
-  response.status(404).json({ error: 'unknown endpoint' })
-})
-
-const errorHandler = (error, request, response, next) => {
-    logger.error(error.message)
-
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-
-    next(error)
-}
-
-app.use(errorHandler)
+app.use(middleware.requestLogger)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
