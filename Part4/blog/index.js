06319db8a800b6d6bd5408e97466ba4e0c7c4db0
David.Kuterna <david.kuterna@ife-doors.com>
2025-12-28 18:23:38 +0100

    4.01

---
+++ Part4/blog/index.js
@@ -0,0 +1,65 @@
+require('dotenv').config()
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+const app = express()
+
+morgan.token('body', (request) => {
+  return JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl, { family: 4 })
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+app.use((request, response)=> {
+  response.status(404).json({ error: 'unknown endpoint' })
+})
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = 3003
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
